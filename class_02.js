09aa3ab4e1c8043ea9d77d27bd6b5826f0e6cc2e
zhang <zhangtuo2008@gmail.com>
2016-08-19 23:26:34 +0200

    exo 2 --class

---
+++ class_02.js
@@ -0,0 +1,73 @@
+/**
+ * Created by tuo on 16/8/19.
+ */
+
+/*
+var Character = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.health_ = 100;
+};
+
+Character.prototype.damage = function() {
+    this.health_ = this.health_ - 10;
+};
+
+Character.prototype.getHealth = function() {
+    return this.health_;
+};
+
+Character.prototype.toString = function() {
+    return "x: " + this.x + " y: " + this.y + " health: " + this.getHealth();
+};
+
+var x = process.argv[2];
+var y = process.argv[3];
+var character = new Character(+x, +y);
+character.damage();
+console.log(character.toString());*/
+
+class Character {
+
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.health_ =100;
+    }
+
+    damage(){
+        this.health_ = this.health_ - 10;
+    }
+
+    getHealth(){
+        return this.health_;
+    }
+
+    toString(){
+        return "x: " + this.x + " y: " + this.y + " health: " + this.getHealth();
+    }
+}
+
+var x = process.argv[2];
+var y = process.argv[3];
+var character = new Character(+x, +y);
+character.damage();
+console.log(character.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
